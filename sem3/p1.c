9d65dbb6dd9d46bf392178aedd6259076bd64e22
Rafal-Raczynski <rafracz@o2.pl>
2020-10-31 17:38:03 +0100

    Zadanka

--- sem3/p1.c
+++ sem3/p1.c
@@ -4,11 +4,10 @@
 int main(int argc, char *argv[])
 {
     float a,b;
-    
-    a=atof(argv[1]);
-    b=atof(argv[2]);
-
        
-
-    printf("Suma:%f\n", a+b);
+    if(argc>1)
+    printf("Suma:%f\n", atof(argv[1])+atof(argv[2]));
+    else if (argc==1)
+    printf("Nie podano ani jednej liczby\n");
+    
 }
